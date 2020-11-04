9ed56506f5b719fbc8a9527461d54adc2b3bbe6e
Дмитрий Горкун <dmitrijgorkun@MacBook.local>
2020-11-05 00:12:05 +0300

    добавление строк в таблицу

--- index.php
+++ index.php
@@ -11,8 +11,37 @@ $range = 'A1:B8';
 $spreadSheetId = '10Ykg87BEbiHlTLFEN9Y-JvrgdczUJ2QzItJEK6XBfUY';
 $responce = $service->spreadsheets_values->get($spreadSheetId, $range);
 $values = $responce->getValues();
+
 if($values) {
     print_r($values);
 } else {
     echo 'no data';
 }
+
+//Insert Data
+
+$range = 'A1:B8';
+
+$values = [
+    ['https://yandex.ru', 'Yandex']
+];
+
+$body = new Google_Service_Sheets_ValueRange([
+    'values' => $values
+]);
+
+$params = [
+    'valueInputOption' => 'RAW'
+];
+
+$input = [
+    'insertDataOption' => 'INSERT_ROWS'
+];
+
+$result = $service->spreadsheets_values->append(
+    $spreadSheetId,
+    $range,
+    $body,
+    $params,
+    $insert
+);
